ff175280662027897a1df3ac571bea6e97b2dfc3
adrian-glz <59483030+adrian-glz@users.noreply.github.com>
2021-07-23 13:21:15 -0600

    UDPATE
    UDPATE

--- src/Frames/Menu.java
+++ src/Frames/Menu.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Frames;
 
 import javax.swing.ImageIcon;
@@ -13,15 +8,12 @@ import javax.swing.ImageIcon;
  */
 public class Menu extends javax.swing.JFrame {
 
-    /**
-     * Creates new form Menu
-     */
     public Menu() {
+        
         initComponents();
          setIconImage(new ImageIcon(getClass().getResource("/recursos/logochico.png")).getImage());
-    }
-
-   
+    }   
+    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
